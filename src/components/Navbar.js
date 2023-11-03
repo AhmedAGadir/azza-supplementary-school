7699944a0647cf100fbf13d2d16a2a89ed0ae2f7
AhmedAGadir <ahmed.gadir@gmail.com>
2023-11-03 23:39:18 +0000

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ import logo from '/public/images/azza-logo.png'
 import { Button } from '@/components/Button'
 import { Icon } from '@/components/Icon'
 
+import kaaf from '/public/images/illustrations/kaaf-purple.svg'
 import sheen from '/public/images/illustrations/sheen-purple.svg'
 
 const navigation = [
@@ -89,7 +90,12 @@ export function Navbar({ programs }) {
               className="absolute inset-x-0 top-0 z-40 w-screen overflow-y-scroll bg-gradient-to-tr from-purple-600 to-purple-600 px-4 py-16 sm:px-8"
             >
               <Image
-                className=" absolute -bottom-20 left-0 -z-20 h-auto w-screen 2xl:block"
+                className=" absolute left-0 top-0 -z-20 h-auto w-3/4 2xl:block"
+                src={kaaf}
+                alt=""
+              />
+              <Image
+                className=" absolute -bottom-20 right-0 -z-20 h-auto w-3/4 2xl:block"
                 src={sheen}
                 alt=""
               />
